15f607be451019e701fb31f9a9e70511b0b057af
Adriel Lucas <adriell.ads@gmail.com>
2017-01-29 18:20:46 -0300

    corrigindo erro na busca de dados do hiera

--- site/profile/manifests/nginx/server.pp
+++ site/profile/manifests/nginx/server.pp
@@ -1,4 +1,11 @@
 class profile::nginx::server {
 	include nginx
+	$nginx_app_name = hiera('profile::nginx::server::app_name')
+        $nginx_server_name = hiera('profile::nginx::server::server_name')
+        nginx::server{"$nginx_app_name":
+                app_name => $nginx_app_name,
+                server_name => $nginx_server_name
+        }
+
 }
 
