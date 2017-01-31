ac32d66d6aac5477ae201e49c9edfe2eb1dfc3b4
Adriel Lucas <adriell.ads@gmail.com>
2017-01-31 12:56:01 -0300

    Alterando o formato de agendamento do crontab

--- site/profile/manifests/cron/jobreport.pp
+++ site/profile/manifests/cron/jobreport.pp
@@ -2,6 +2,6 @@ class profile::cron::jobreport{
 	include utils
 	
 	exec {"scheduleJob":
-                command => '(/usr/bin/crontab -l; echo "00 12 * * * sh /opt/jobreport.sh") | crontab - '
+                command => '/usr/bin/sh /opt/schedule.sh'
         }
 }
