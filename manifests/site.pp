055ccd9b7d708f2bac112d008832de2387bcfa9b
Adriel Lucas <adriell.ads@gmail.com>
2017-01-30 23:05:36 -0300

    ajustes no role::app

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,5 +3,6 @@ node "chaordicapp.lab"{
 	include role::reverseproxy
 	include role::nodejs
 	include role::pm2
+	include role::app
 
 }
--- site/profile/manifests/app/deploy.pp
+++ site/profile/manifests/app/deploy.pp
@@ -1,15 +1,14 @@
 class profile::app::deploy{
-	include profile::nodejs::package
-	include profile::pm2::install
-	class{'vcsrepo':}
-	vcsrepo { '/var/www/html':
- 		ensure   => present,
-  		provider => git,
-		source   => 'https://gitlab.com/chaordic/chaordicapp.git',
-	}
-	exec {"startApp":
-		path   	=> '/usr/bin:/usr/sbin:/bin',
-	  	onlyif 	=> 'test -f /var/www/html/chaordicapp/app.js',
-		command => 'pm2 start /var/www/html/chaordicapp/app.js'
-	}
+        include profile::nodejs::package
+        include profile::pm2::install
+        vcsrepo { '/var/www/html/production':
+                ensure   => present,
+                provider => git,
+                source   => 'https://gitlab.com/chaordic/chaordicapp.git',
+        }
+
+        exec {"startApp":
+                command => '/usr/local/bin/pm2 start /var/www/html/production/app.js'
+        }
 }
+
