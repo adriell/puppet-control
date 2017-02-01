073219a19da8bdb8abf144e5269c233b8f0d4934
Adriel Lucas <adriell.ads@gmail.com>
2017-02-01 17:24:40 -0300

    corrigindo o nome do arquivo

--- site/profile/manifests/nginx/server.pp
+++ site/profile/manifests/nginx/server.pp
@@ -6,8 +6,8 @@ class profile::nginx::server {
 		ensure => present,
 	}
 	
-	$nginx_app_name = hiera('profile::nginx::server::app_name')
-        $nginx_server_name = hiera('profile::nginx::server::server_name')
+	$nginx_app_name = "app"
+        $nginx_server_name = "localhost"
         nginx::server{"$nginx_app_name":
                 app_name => $nginx_app_name,
                 server_name => $nginx_server_name
